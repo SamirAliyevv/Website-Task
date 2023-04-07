8c393d8a3e5237759e1b2625b0f47ee413718323
SamirAliyevv <samirsa@code.edu.az>
2023-04-07 22:23:02 +0400

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,23 @@
 var languagesdiv = document.querySelector('.languages');
 var arrow = document.querySelector('#arrow');
+var searchicon = document.querySelector('#axtarbutton')
+var dn =  document.querySelector('#inp')
+var dni =  document.querySelector('#inp').value;
+var xclose= document.querySelector('#x')
+var barmenu = querySelector('#bar')
+
+    
+
+    
+
+searchicon.onclick = function(){
+     dn.classList.toggle('active')
+     xclose.classList.toggle('active')
+}   
+xclose.onclick = function(){
+    dn.classList.toggle('active') 
+    this.classList.toggle('active')  
+}
 
 arrow.onclick= function() {
     languagesdiv.classList.toggle('active')
